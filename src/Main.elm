3989eadb9135ae61ab5f7d0ee36caf37013c7871
Michael <michael@metaset.com.au>
2016-10-27 19:20:03 +1100

    Updates.

--- src/Main.elm
+++ src/Main.elm
@@ -2,7 +2,8 @@ import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.App exposing (..)
 import Html.Events exposing (onClick)
-import Array exposing (Array)
+import Array exposing (..)
+import List exposing (..)
 import String
 
 import Greeting exposing (sayHello, sayGoodbye)
@@ -36,21 +37,36 @@ initialModel =
 -- Nav
 
 
-pageNav : Array String
-pageNav =
-  Array.fromList
-    initialModel.nav
+type alias Nav =
+  { item : String }
+
+
+renderNav item =
+  li [] [ text item ]
+
+
+
+renderMyList :  String -> Html a
+renderMyList item =
+  li [ class "blah" ] [ text ( "item: " ++ item ) ]
+
+myList = ["one","two","three"]
 
 
 -- View
 
 view : Model -> Html Msg
 view model =
-  div [ id "app", class ( model.appColor ) ] [
-    p [ class "print-out" ] [ text ( toString model ) ]
-    , p [ class "headline" ] [ text (sayGoodbye "Eric") ]
-    , button [ type' "button", onClick ChangeAppColor ] [ text "Press me" ]
-  ]
+  ul [] [
+    ( List.map renderMyList myList )
+    ]
+
+-- div [ id "app", class ( model.appColor ) ] [
+--   (List.map renderMyList myList)
+--   , p [ class "print-out" ] [ text ( toString model ) ]
+--   , p [ class "headline" ] [ text ( sayGoodbye "Eric" ) ]
+--   , button [ type' "button", onClick ChangeAppColor ] [ text "Press me" ]
+-- ]
 
 
 -- Update
--- src/ui/Greeting.elm
+++ src/ui/Greeting.elm
@@ -2,10 +2,12 @@ module Greeting exposing (..)
 
 -- Say hello
 
+sayHello : String -> String
 sayHello name =
   "Hello " ++ name
 
 -- Say goodbye
 
+sayGoodbye : String -> String
 sayGoodbye name =
   "Goodbye " ++ name
